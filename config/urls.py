45f9ecd344e9481e605c2183bfddd2d861bc0385
SolioKyaiz <jonesbonked@gmail.com>
2025-03-07 17:56:52 +0600

    Added sorting and filters

--- config/urls.py
+++ config/urls.py
@@ -21,11 +21,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from tasks.views import task_list
+from tasks.views import home
 
 
 urlpatterns = [
-    path('',task_list, name = 'home' ),
+    path('',home, name = 'home' ),
     path('admin/', admin.site.urls),
     path('account/', include('account.urls', namespace="account")),
     path('tasks/', include('tasks.urls', namespace="tasks"))
